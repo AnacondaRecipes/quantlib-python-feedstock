7e8773a41b2d0214fd2a7a44173d02321b8888b1
Anton Kabanovs <akabanovs@anaconda.com>
2023-06-12 19:20:12 +0100

    Try changing libboost lib name

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,6 +1,11 @@
 set BOOST_ROOT=%LIBRARY_PREFIX%
 set QL_DIR=%LIBRARY_PREFIX%
 
+echo "BOOST_VER: %BOOST_VER%"
+echo "VC_VER: %VC_VER%"
+copy "%LIBRARY_PREFIX%\libboost_serialization.lib" "%LIBRARY_PREFIX%\libboost_serialization-vc141-mt-x64-1_73.lib"
+if %ERRORLEVEL% GEQ 1 exit 1
+
 cd Python
 python setup.py wrap
 python setup.py build
